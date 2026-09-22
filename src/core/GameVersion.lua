21d85234364d5ea9e606d297237f78c0d6b07e08
roccordx <roccordx@gmail.com>
2026-09-22 15:00:22 +0000

    feat: integrate Italian Red ROM manifest

--- src/core/GameVersion.lua
+++ src/core/GameVersion.lua
@@ -26,6 +26,16 @@ GameVersion.VERSIONS = {
     cachePrefix = "",       -- Red owns the cache root (backwards compatible)
     saveSuffix = "",        -- save.lua / save.lua.bak / save.lua.tmp
   },
+  red_ita = {
+    id = "red_ita",
+    label = "Red IT",
+    displayName = "Pokemon Red Italiano",
+    launcherName = "Red Italiano",
+    sha1 = "65b97cf8f2f1cff711a6d08c6c894c8ce65ce522",
+    manifest = "tools/rom_variants/red_ita_manifest.json",
+    cachePrefix = "red_ita/",
+    saveSuffix = "_red_ita",
+  },
   blue = {
     id = "blue",
     label = "Blue",
